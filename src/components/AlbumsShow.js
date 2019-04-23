96e277af42466b2d6feb50f50e29acba9201fca7
Emma Price <ekprice01@gmail.com>
2019-04-23 14:14:53 +0100

    Updated albumsshow component

--- src/components/AlbumsShow.js
+++ src/components/AlbumsShow.js
@@ -19,7 +19,26 @@ class AlbumsShow extends React.Component {
   render() {
     if(!this.state.album) return null
     return (
-      <div>{this.state.album.title}</div>
+      <section className="section">
+        <div className="container">
+          <h1 className="title is-1">{this.state.album.title}</h1>
+          <hr />
+          <div className="columns">
+            <div className="column is-half-desktop is-full-tablet is-full-mobile">
+              <figure className="image">
+                <img src={this.state.album.cover_medium} alt={this.state.album.title} />
+              </figure>
+            </div>
+            <div className="column is-half-desktop is-full-tablet is-full-mobile">
+              <h2 className="title is-4">Artist: {this.state.album.artist.name}</h2>
+              <h2 className="title is-4">Label: {this.state.album.label}</h2>
+              <h2 className="title is-4">Number of tracks: {this.state.album.nb_tracks}</h2>
+              <h2 className="title is-4">Release date: {this.state.album.release_date}</h2>
+              <h2 className="title is-4">Genre: {this.state.album.genres.data[0].name}</h2>
+            </div>
+          </div>
+        </div>
+      </section>
     )
   }
 }
