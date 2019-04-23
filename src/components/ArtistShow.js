f48bd818d0fc4a6572b24e3d9decf48143939748
Emma Price <ekprice01@gmail.com>
2019-04-23 12:03:34 +0100

    Got axios request to work

--- src/components/ArtistShow.js
+++ src/components/ArtistShow.js
@@ -1,20 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 class ArtistShow extends React.Component {
   constructor(){
     super()
 
     this.state = {
-
+      artists: null
     }
   }
 
-
+  componentDidMount(){
+    axios.get('https://cors-anywhere.herokuapp.com/api.deezer.com/search?q=coldplay')
+      .then(res => this.setState({ artists: res.data.data }))
+  }
   render() {
+    console.log(this.state.artists)
     return(
-      <div>hi</div>
+      <div></div>
     )
   }
 }
