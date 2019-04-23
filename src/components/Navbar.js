8dc797209fdcf74ddeb47d1ba84a1337b9bfda85
Emma Price <ekprice01@gmail.com>
2019-04-23 15:01:01 +0100

    Searchbar is still working...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,69 @@
 import React from 'react'
-import { Link} from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
-const Navbar = () => {
-  return (
-    <nav className="navbar is-light">
-      <div className="container">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item">Home</Link>
+class Navbar extends React.Component {
 
-          <a role="button" className="navbar-burger">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
+  constructor() {
+    super()
 
-        </div>
+    this.state = {
+      data: {}
+    }
+    this.handleSearch =this.handleSearch.bind(this)
+  }
 
-        <div className="navbar-menu">
-          <div className="navbar-start">
-            <Link to="/albums" className="navbar-item">Albums</Link>
+  handleSearch(e) {
+    const data=[e.target.value]
+    this.setState({ data })
+    console.log(this.state.data)
+  }
+
+
+
+  render() {
+    return (
+      <nav className="navbar is-light">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">Home</Link>
+
+            <a role="button" className="navbar-burger">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+
           </div>
 
-          <div className="navbar-end">
-            <div className="navbar-item">
+          <div className="navbar-menu">
+            <div className="navbar-start">
+              <Link to="/albums" className="navbar-item">Albums</Link>
+            </div>
 
-              <form>
-                <div className="field has-addons">
-                  <div className="control">
-                    <input className="input" type="text" placeholder="Find a repository" />
-                  </div>
-                  <div className="control">
-                    <a className="button is-info">
-                    Search
-                    </a>
-                  </div>
-                </div>
-              </form>
+            <div className="navbar-end">
+              <div className="navbar-item">
 
+                <form>
+                  <div className="field has-addons">
+                    <div className="control">
+                      <input className="input" type="search" onChange={this.handleSearch} placeholder="Artist Name..." />
+                    </div>
+                    <div className="control">
+                      <a className="button is-info">
+                      Search
+                      </a>
+                    </div>
+                  </div>
+                </form>
+
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
-  )
+      </nav>
+    )
+  }
 }
 
 
-export default Navbar
+export default withRouter(Navbar)
