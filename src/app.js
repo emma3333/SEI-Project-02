1609d27d15c5b686dc61c0db7a86848ea39d7494
Emma Price <ekprice01@gmail.com>
2019-04-23 10:50:47 +0100

    Added home page

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ console.log('JS loaded')
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Home from './components/Home'
+
 class App extends React.Component {
   render() {
     return(
       <main>
         <div> HI! </div>
+        <Home />
       </main>
     )
   }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title is-1">Music Search!</h1>
+        <h2 className="subtitle is-2">Look for your artists</h2>
+      </div>
+    </section>
+  )
+}
+
+export default Home
