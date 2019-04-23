cc645935ec061cab90eb9c071ade9318d63e9483
Emma Price <ekprice01@gmail.com>
2019-04-23 14:26:35 +0100

    Added search bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,6 +22,22 @@ const Navbar = () => {
           </div>
 
           <div className="navbar-end">
+            <div className="navbar-item">
+
+              <form>
+                <div className="field has-addons">
+                  <div className="control">
+                    <input className="input" type="text" placeholder="Find a repository" />
+                  </div>
+                  <div className="control">
+                    <a className="button is-info">
+                    Search
+                    </a>
+                  </div>
+                </div>
+              </form>
+
+            </div>
           </div>
         </div>
       </div>
